ae47d3ef6a97cbe5c254f7b6b51843ce71395612
Leomin07 <tranminhsvp@gmail.com>
2021-09-12 21:06:59 +0700

    init

--- order/src/components/Checkout/Checkout.js
+++ order/src/components/Checkout/Checkout.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { ImLocation2 } from 'react-icons/im';
 
 const Checkout = () => {
   return (
-    <div className="user-info">
-      <h2>checkoiuts</h2>
+    <div className="checkout-address">
+      <h3 className="checkout-info_title">
+        <ImLocation2 size="2em" />
+        ĐỊA CHỈ NHẬN HÀNG
+      </h3>
+      <div className="checkout-address-user">
+        <div className="checkout-user__name">tranminh</div>
+        <div className="checkout__mobile">09765</div>
+      </div>
+      <div className="checkout-address-inner">hoang quoc viet</div>
     </div>
   );
 };
