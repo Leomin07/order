710c9e680e3047cf2d458065739a916f2bdcef69
Tran Dai Minh <61413434+Leomin07@users.noreply.github.com>
2021-09-14 09:17:11 +0700

    Delete order directory

--- order/src/components/Admin/HeaderAdmin.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const HeaderAdmin = () => {
-  return <div>header admin</div>;
-};
-
-export default HeaderAdmin;
--- order/src/types/cartType.js
+++
@@ -1,9 +0,0 @@
-export const CART_LIST = 'CART_LIST';
-export const ADD_TO_CART = 'ADD_TO_CART';
-export const INCREASE_QTY = 'INCREASE_QTY';
-export const DECREASE_QTY = 'DECREASE_QTY';
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
-export const REMOVE_MULTI_ITEM = 'REMOVE_MULTI_ITEM';
-export const EMPTY_CART = 'EMPTY_CART';
-export const COMPLETE_CART = 'COMPLETE_CART';
-export const COMPLETE_ALL_CART = 'COMPLETE_ALL_CART';
